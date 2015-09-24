9660a86e41f759bd280a03a147190693f2e0a9b5
Mg <gamem.8818@gmail.com>
2015-09-24 16:07:57 -0400

    add 104

---
+++ 104_MaximumDepthofBinaryTree.rb
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {Integer}
+def max_depth(root)
+    if root.nil?
+        return 0
+    end
+    return 1+ [max_depth(root::left), max_depth(root::right)].max
+end
