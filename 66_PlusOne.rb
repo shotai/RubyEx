7a1f34481d3fe72a75dbeb7dc9c50c19e548db02
Mg <gamem.8818@gmail.com>
2015-09-24 16:57:56 -0400

    collect->map

--- 66_PlusOne.rb
+++ 66_PlusOne.rb
@@ -1,7 +1,7 @@
 # @param {Integer[]} digits
 # @return {Integer[]}
 def plus_one(digits)
-    tmp = (digits.collect!{|x| x.to_s}.join.to_i) +1
-    tmp2 = tmp.to_s.split("").collect!{|x| x.to_i}
+    tmp = (digits.map!{|x| x.to_s}.join.to_i) +1
+    tmp2 = tmp.to_s.split("").map!{|x| x.to_i}
     return tmp2
 end
